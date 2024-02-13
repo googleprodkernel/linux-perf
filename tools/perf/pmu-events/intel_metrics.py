8bad2ea53d461e40d786374c20ee18ca799f7825
Ian Rogers <irogers@google.com>
2024-02-12 22:18:36 -0800

    perf jevents: Add load event json to verify and allow fallbacks
    Add a LoadEvents function that loads all event json files in a
    directory. In the Event constructor ensure all events are defined in
    the event json except for legacy events like "cycles". If the initial
    event isn't found then legacy_event1 is used, and if that isn't found
    legacy_event2 is used. This allows a single Event to have multiple
    event names as models will often rename the same event over time. If
    the event doesn't exist an exception is raised.
    
    So that references to metrics can be added, add the MetricRef
    class. This doesn't validate as an event name and so provides an
    escape hatch for metrics to refer to each other.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/pmu-events/intel_metrics.py
+++ tools/perf/pmu-events/intel_metrics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
-from metric import (JsonEncodeMetric, JsonEncodeMetricGroupDescriptions, MetricGroup)
+from metric import (JsonEncodeMetric, JsonEncodeMetricGroupDescriptions, LoadEvents,
+                    MetricGroup)
 import argparse
 import json
 import os
@@ -26,6 +27,9 @@ def main() -> None:
       help='Root of tree containing architecture directories containing json files'
   )
   _args = parser.parse_args()
+
+  directory = f"{_args.events_path}/x86/{_args.model}/"
+  LoadEvents(directory)
 
   all_metrics = MetricGroup("",[])
 
