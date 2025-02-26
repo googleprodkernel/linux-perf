d9d5eb531b4255a00adb94d4bb4456d814c86e1c
Ian Rogers <irogers@google.com>
2025-02-26 14:55:55 -0800

    perf tests: Fix data symbol test with LTO builds
    With LTO builds, although regular builds could also see this as
    all the code is in one file, the datasym workload can realize the
    buf1.reserved data is never accessed. The compiler moves the
    variable to bss and only keeps the data1 and data2 parts as
    separate variables. This causes the symbol check to fail in the
    test. Make the variable volatile to disable the more aggressive
    optimization. Rename the variable to make which buf1 in perf is
    being referred to.
    
    Before:
    ```
    $ perf test -vv "data symbol"
    126: Test data symbol:

--- tools/perf/tests/workloads/datasym.c
+++ tools/perf/tests/workloads/datasym.c
@@ -7,7 +7,8 @@ typedef struct _buf {
 	char data2;
 } buf __attribute__((aligned(64)));
 
-static buf buf1 = {
+/* volatile to try to avoid the compiler seeing reserved as unused. */
+static volatile buf workload_datasym_buf1 = {
 	/* to have this in the data section */
 	.reserved[0] = 1,
 };
@@ -15,8 +16,8 @@ static buf buf1 = {
 static int datasym(int argc __maybe_unused, const char **argv __maybe_unused)
 {
 	for (;;) {
-		buf1.data1++;
-		if (buf1.data1 == 123) {
+		workload_datasym_buf1.data1++;
+		if (workload_datasym_buf1.data1 == 123) {
 			/*
 			 * Add some 'noise' in the loop to work around errata
 			 * 1694299 on Arm N1.
@@ -30,9 +31,9 @@ static int datasym(int argc __maybe_unused, const char **argv __maybe_unused)
 			 * longer a continuous repeating pattern that interacts
 			 * badly with the bias.
 			 */
-			buf1.data1++;
+			workload_datasym_buf1.data1++;
 		}
-		buf1.data2 += buf1.data1;
+		workload_datasym_buf1.data2 += workload_datasym_buf1.data1;
 	}
 	return 0;
 }
