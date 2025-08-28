d9cbad21a62fbe5ce4132f4dbd20720ed10c721b
Ian Rogers <irogers@google.com>
2025-08-28 13:59:29 -0700

    perf print-events: Remove print_symbol_events
    Now legacy hardware events are in json there's no need for a specific
    printing routine that previously served for both hardware and software
    events. The associated event_symbols_hw is also removed. To support
    the previous filtered version use an event glob of "legacy hardware"
    which matches the topic of the json events.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/util/parse-events.h
+++ tools/perf/util/parse-events.h
@@ -264,7 +264,6 @@ struct event_symbol {
 	const char	*symbol;
 	const char	*alias;
 };
-extern const struct event_symbol event_symbols_hw[];
 
 char *parse_events_formats_error_string(char *additional_terms);
 
